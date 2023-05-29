8cf2a9e2fa30315313391885ae12b6d86cb7b7c1
TimeX4 <75556349+TimeX4@users.noreply.github.com>
2023-05-29 11:40:38 -0400

    Behdad beta testing bug finds

--- src/main/java/org/library/Main.java
+++ src/main/java/org/library/Main.java
@@ -6,13 +6,17 @@ import org.gui.LibraryGUI;
 
 public class Main {
     public static void main(String[] args) {
-        // TODO: 2023-05-24 BUG TESTING: Invalid type checking on fields, unexpected input, large
-        // values, small values, wrapper class for number conversion
         // TODO: 2023-05-24 find out where notifiers are needed in the gui!
         /* BUGS
-        Type checking across all input fields is unhandled
+        Invalid type checking on fields, unexpected input, large values, small values, wrapper class for number conversion, escape characters
+        empty strings,
         Edit on AllItemsUser when nothing is present or selected throws error
-
+        Don't allow books with same name and author unless reference only is different
+        Don't allow patrons with the same phone number <- add notifier, phone number already registed
+        When editing passwords, if password field isn't changed, it double hashes
+        All library items page, buttons doesn't have null (nothing selected) check
+        Add button on patron and books needs to be validated before adding, currently it adds empty strings
+        When patron gets deleted, ensure all of their books are checked back in first
          */
         LibraryGUI.draw();
     }
